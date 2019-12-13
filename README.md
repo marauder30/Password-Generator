fe1735331e61b0a9e68a2072cb124bb53009449a
Nick Koch <marauder30@gmail.com>
2019-12-12 20:18:41 -0700

    began readme, added nav and buttons to html

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Password-Generator
+
+INTRODUCTION
+---------------------------------------------
+
+The purpose of this activity is to build a functional random password generator. The application will take a set of choices defined by the user to create a unique password between 8 and 128 characters.
---
+++ script.js
@@ -0,0 +1,22 @@
+//button eventlistener when clicked will generate password 
+
+//prompt or slider "please define a value between 8-128 to determine how long your 
+//  password will be."
+
+//prompt or checkbox "do you want to use upper case letters?"
+
+//prompt  or checkbox "do you want to use lower case letters?"
+
+//prompt  or checkbox "do you want to use numbers?"
+
+//prompt or checkbox "do you want to use special characters?"
+
+//user must specify at least one character 
+
+
+
+
+
+//write password to page using appendChild
+
+//second button to 
